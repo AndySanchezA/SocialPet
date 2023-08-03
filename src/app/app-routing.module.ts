76c8707f7c4c20a9bce26f123d4678495e461266
AndySanchezA <76460870+AndySanchezA@users.noreply.github.com>
2023-08-03 15:32:39 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((m) => m.SignupPageModule),
+  },
+  {
+    path: 'llenar-informacion',
+    loadChildren: () =>
+      import('./llenar-informacion/llenar-informacion.module').then(
+        (m) => m.LlenarInformacionPageModule
+      ),
+  },
+  {
+    path: 'informacion-modal',
+    loadChildren: () => import('./informacion-modal/informacion-modal.module').then( m => m.InformacionModalPageModule)
+  },
+  {
+    path: 'image-viewer-modal',
+    loadChildren: () => import('./image-viewer-modal/image-viewer-modal.module').then( m => m.ImageViewerModalPageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'user-service',
+    loadChildren: () => import('./user-service/user-service.module').then( m => m.UserServicePageModule)
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
